15785b5a5f677103502ab8771e3f354a19f10eba
Rony Cuzco <ronald.cuzco@ciberexperis.es>
2024-04-28 19:22:42 +0200

    - Se corrige código para poder mostrar imágenes y mostrar correctamente página de presupuestos.

--- src/app/services/global-data.service.ts
+++ src/app/services/global-data.service.ts
@@ -2,30 +2,47 @@ import { Injectable } from '@angular/core';
 import { Cliente } from '../models/cliente';
 
 @Injectable({
-  providedIn: 'root'
+    providedIn: 'root'
 })
-export class GlobalDataService {
+export class GlobalDataService
+{
 
-  private usuarioLogado!:Cliente | undefined|null ;
+    private usuarioLogado!: Cliente | undefined | null;
 
-  constructor() { }
+    constructor() { }
 
-  setUsuarioLogado(cliente:Cliente){
-    this.usuarioLogado = cliente;
-  //save to local storage
-    localStorage.setItem('usuarioLogado', JSON.stringify(cliente));
+    setUsuarioLogado(cliente: Cliente)
+    {
+        this.usuarioLogado = cliente;
+        //save to local storage
+        localStorage.setItem('usuarioLogado', JSON.stringify(cliente));
 
-  }
-  getUsuarioLogado():Cliente|undefined|null{
-    //read from local storage, if empty then return null
-    const storedUser = localStorage.getItem('usuarioLogado');
-    //console.log("storedUser",storedUser);
-    if (storedUser) {
-        return JSON.parse(storedUser);
-    } else {
-        return null;
+    }
+    getUsuarioLogado(): Cliente | undefined | null
+    {
+        //read from local storage, if empty then return null
+        const storedUser = localStorage.getItem('usuarioLogado');
+        //console.log("storedUser",storedUser);
+        if (storedUser)
+        {
+            return JSON.parse(storedUser);
+        } else
+        {
+            return null;
+        }
     }
 
+    getIdPresupuestoActual(): number | null
+    {
+        //read from local storage, if empty then return null
+        const budgetId = localStorage.getItem('budgetId');
 
-  }
+        if (budgetId)
+        {
+            return parseInt(budgetId);
+        } else
+        {
+            return null;
+        }
+    }
 }
